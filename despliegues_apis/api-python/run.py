290c89a4cb3185f466e11b0df6c2e5b224bb6201
Yair Lopez <yahirlopez380@gmail.com>
2026-02-17 10:33:02 -0600

    cambio de puerto api python

--- despliegues_apis/api-python/run.py
+++ despliegues_apis/api-python/run.py
@@ -6,5 +6,5 @@ app = create_app()
 
 if __name__ == '__main__':
     # Usa PORT del entorno cuando esta disponible (por ejemplo, despliegue).
-    port = int(os.environ.get("PORT", 3000))
+    port = int(os.environ.get("PORT", 3500))
     app.run(host='0.0.0.0', port=port)
